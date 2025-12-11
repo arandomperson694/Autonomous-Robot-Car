17ee436bc6b61a5b9a542ab7a8dd6071048645b7
arandomperson694 <148708451+arandomperson694@users.noreply.github.com>
2025-12-11 20:20:29 +0000

    Implement CSV reading and image loading
    Added CSV handling and image loading for model training.

--- Model Creater.py
+++ Model Creater.py
@@ -1,22 +1,43 @@
 #Model creator - TensorFlow
 
-
 # Import libraries
 
-import tensorflow
+import random
+import csv
+import tensorflow as tf
+import cv2
+
+
+# Variables
+
+IMG_DIR = "data" # Data structure is raw images and CSV in one folder called data for simplicity
+CSV_PATH = "data"
+
+DATA_SPLIT = 20 # Percentage of data used for validation
 
 
 
-# Open CSV file and load image folder
+# Open CSV file
+
+with open(CSV_PATH, "r") as file:
+
+  telemetry = csv.reader(file)
+  print(CSV_PATH, "opened")
 
 
+  # Loop - feed image and telemetry into TensorFlow model for each csv line (model.fit)
+  
+  for row in telemetry:
 
-# Split 80% for training, 20% for validation data
+    imageName = row[1]
+    steering = row[2]
+    throttle = row[3]
 
+    
+    image = cv2.imread(imageName) # image = cv2.imread(imageName, 0) for greyscale
 
-
-
-# Loop - feed image and telemetry into TensorFlow model for each csv line (model.fit)
+    
+  
 
 
 
